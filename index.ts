370355a00d52ad48ef1de359435af1f0a583a002
Jon Gunter <jon@jongunter.com>
2018-07-01 21:36:30 -0600

    Create a simple model

--- index.ts
+++ index.ts
@@ -1,27 +1,33 @@
 import * as tf from '@tensorflow/tfjs';
 
-// const initialValues = tf.zeros([5]);
-// const biases = tf.variable(initialValues);
-// biases.print();
+const a = tf.scalar(2);
+const b = tf.scalar(4);
+const c = tf.scalar(8);
 
-// const updatedValues = tf.tensor1d([0, 1, 0, 1, 0]);
-// biases.assign(updatedValues);
-// biases.print();
+function predict (input: number): tf.Tensor {
+    return tf.tidy(() => {
+        const x = tf.scalar(input);
 
-const d = tf.tensor2d([[1.0, 2.0], [3.0, 4.0]]);
-const d_squared = d.square();
-d_squared.print();
+        const ax2 = a.mul(x.square());
+        const bx = b.mul(x);
+        const y = ax2.add(bx).add(c);
 
-const e = tf.tensor2d([
-    [1.0, 2.0],
-    [3.0, 4.0]
-]);
-const f = tf.tensor2d([
-    [5.0, 6.0],
-    [7.0, 8.0]
-]);
-const e_plus_f = e.add(f);
-e_plus_f.print();
+        return y;
+    });
+}
 
-const sq_sum = e.add(f).square();
-sq_sum.print();
+function predictSoemthingElse(input: number): tf.Tensor {
+    return tf.tidy(() => {
+        const ten = tf.scalar(10);
+        return tf.scalar(input).mul(ten);
+    });
+}
+
+const result = predict(2);
+result.print();
+
+const result2 = predict(4);
+result2.print();
+
+const result3 = predictSoemthingElse(5);
+result3.print();
