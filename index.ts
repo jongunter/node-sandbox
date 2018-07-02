1fc1ee553f1627b9549bbc0a13b2701572bdfaef
Jon Gunter <jon@jongunter.com>
2018-07-01 21:41:41 -0600

    Play with more models - broke it oops

--- index.ts
+++ index.ts
@@ -1,33 +1,13 @@
 import * as tf from '@tensorflow/tfjs';
 
-const a = tf.scalar(2);
-const b = tf.scalar(4);
-const c = tf.scalar(8);
+const LEARNING_RATE = 100;
 
-function predict (input: number): tf.Tensor {
-    return tf.tidy(() => {
-        const x = tf.scalar(input);
+const model = tf.sequential();
+const layer = tf.layers.simpleRNN({
+    units: 20, 
+    recurrentInitializer: 'GlorotNormal',
+    inputShape: [80, 4]
+});
+model.add(layer);
 
-        const ax2 = a.mul(x.square());
-        const bx = b.mul(x);
-        const y = ax2.add(bx).add(c);
-
-        return y;
-    });
-}
-
-function predictSoemthingElse(input: number): tf.Tensor {
-    return tf.tidy(() => {
-        const ten = tf.scalar(10);
-        return tf.scalar(input).mul(ten);
-    });
-}
-
-const result = predict(2);
-result.print();
-
-const result2 = predict(4);
-result2.print();
-
-const result3 = predictSoemthingElse(5);
-result3.print();
+const optimizer = tf.train.sgd(LEARNING_RATE);
