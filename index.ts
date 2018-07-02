35b373fae44db95fd91c28f50d93cd91ec878ff6
Jon Gunter <jon@jongunter.com>
2018-07-01 21:16:58 -0600

    Try variables

--- index.ts
+++ index.ts
@@ -1,17 +1,9 @@
 import * as tf from '@tensorflow/tfjs';
 
-const shape = [2, 3];
-const a = tf.tensor([1.0, 2.0, 3.0, 10.0, 20.0, 30.0], shape);
-a.print();
+const initialValues = tf.zeros([5]);
+const biases = tf.variable(initialValues);
+biases.print();
 
-const b = tf.tensor([[1.0, 2.0, 3.0], [10.0, 20.0, 30.0]]);
-b.print();
-
-const c = tf.tensor2d([
-    [1.0, 2.0, 3.0],
-    [10.0, 20.0, 30.0]
-]);
-c.print();
-
-const zeros = tf.zeros([3, 5]);
-zeros.print();
+const updatedValues = tf.tensor1d([0, 1, 0, 1, 0]);
+biases.assign(updatedValues);
+biases.print();
