c4eca7f5edbcc2f8e72cc1b857b339394d9b7836
Jon Gunter <jon@jongunter.com>
2018-07-01 20:57:07 -0600

    Get it working

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-const message: string = 'Hello world'
-console.log(message);
+import * as tf from '@tensorflow/tfjs';
+
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+model.fit(xs, ys, {epochs: 10}).then(() => {
+    const result = model.predict(tf.tensor2d([5], [1, 1]));
+    console.log(result);
+});
